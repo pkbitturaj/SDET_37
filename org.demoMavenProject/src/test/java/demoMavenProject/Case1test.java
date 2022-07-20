30dd2670ef78e27ec94e6fe64129963c46b2cf48
hp <hp@DESKTOP-6HMH41Q>
2022-07-20 16:13:22 +0530

    4.13 commit

--- org.demoMavenProject/src/test/java/demoMavenProject/Case1test.java
+++ org.demoMavenProject/src/test/java/demoMavenProject/Case1test.java
@@ -4,8 +4,14 @@ import org.testng.annotations.Test;
 import org.testng.Reporter;
 
 public class Case1test {
+	
+
 	@Test
 	public void testTc1( ) {
+		String Browser=System.getProperty("browser");
+		String Url = System.getProperty("url");
+		System.out.println(Browser+","+Url);
+		
 		Reporter.log("hello selenium",true);
 
 	}
